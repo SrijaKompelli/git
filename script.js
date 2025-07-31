62dfecae114a2545a25e938f9f234ff7c9aebbbc
Srija <srijakompelli@gmail.com>
2025-07-31 14:00:48 +0530

    Removed unwantedfile.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-  const name = document.getElementById("name").value;
-  if (!name.trim()) {
-    alert("Name cannot be empty!");
-    return false;
-  }
-  return true;
-}
